53ae518dfbcd929215c1c4c2f2ec0f8dd69cd601
Filip Stepien <contact@filipstepien.com>
2018-07-07 09:17:46 -0400

    shopping list connected to server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const items = require("./routes/api/items");
 
@@ -16,6 +17,13 @@ mongoose
   .catch(err => console.log(err));
 
 app.use("/api/items", items);
+
+if (process.env.NOVE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
